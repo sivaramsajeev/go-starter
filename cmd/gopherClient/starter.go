a474b9864a6166acff5bc701d0bbc3f2bb01c6f8
Sivaram Sajeev <sivaramsajeev@gmail.com>
2020-10-01 23:29:52 +0530

    cli development started

--- cmd/gopherClient/starter.go
+++ cmd/gopherClient/starter.go
@@ -4,10 +4,24 @@ import (
 	"fmt"
 	"github.com/sivaramsajeev/go-starter/dirs"
 	"github.com/sivaramsajeev/go-starter/internal/auth"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+
+var (
+  debug   = kingpin.Flag("debug", "Enable debug mode.").Bool()
+  timeout = kingpin.Flag("timeout", "Timeout waiting for ping.").Default("5s").OverrideDefaultFromEnvar("PING_TIMEOUT").Short('t').Duration()
+  ip      = kingpin.Arg("ip", "IP address to ping.").Required().IP()
+  count   = kingpin.Arg("count", "Number of packets to send").Int()
 )
 
 func main() {
 	fmt.Println("Gopher z Just starting out :)")
 	fmt.Println(auth.GetAuth())
 	fmt.Println(dirs.AddonPrint())
+
+
+  kingpin.Version("0.0.1")
+  kingpin.Parse()
+  fmt.Printf("Would ping: %s with timeout %s and count %d\n", *ip, *timeout, *count)
 }

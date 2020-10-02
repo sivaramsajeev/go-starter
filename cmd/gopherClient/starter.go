814fffca1450ede1b47b47ef324f9ba5c048a092
Sivaram Sajeev <sivaramsajeev@gmail.com>
2020-10-02 12:43:15 +0530

    Colorized

--- cmd/gopherClient/starter.go
+++ cmd/gopherClient/starter.go
@@ -7,12 +7,11 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-
 var (
-  debug   = kingpin.Flag("debug", "Enable debug mode.").Bool()
-  timeout = kingpin.Flag("timeout", "Timeout waiting for ping.").Default("5s").OverrideDefaultFromEnvar("PING_TIMEOUT").Short('t').Duration()
-  ip      = kingpin.Arg("ip", "IP address to ping.").Required().IP()
-  count   = kingpin.Arg("count", "Number of packets to send").Int()
+	debug   = kingpin.Flag("debug", "Enable debug mode.").Bool()
+	timeout = kingpin.Flag("timeout", "Timeout waiting for ping.").Default("5s").OverrideDefaultFromEnvar("PING_TIMEOUT").Short('t').Duration()
+	ip      = kingpin.Arg("ip", "IP address to ping.").Required().IP()
+	count   = kingpin.Arg("count", "Number of packets to send").Int()
 )
 
 func main() {
@@ -20,8 +19,7 @@ func main() {
 	fmt.Println(auth.GetAuth())
 	fmt.Println(dirs.AddonPrint())
 
-
-  kingpin.Version("0.0.1")
-  kingpin.Parse()
-  fmt.Printf("Would ping: %s with timeout %s and count %d\n", *ip, *timeout, *count)
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+	fmt.Printf("Would ping: %s with timeout %s and count %d\n", *ip, *timeout, *count)
 }
--- dirs/addon.go
+++ dirs/addon.go
@@ -1,11 +1,42 @@
 package dirs
 
-// import (
-// 	"fmt"
-// )
-
-
+import (
+	"github.com/fatih/color"
+	"fmt"
+)
 
 func AddonPrint() string {
+	color.Cyan("Prints text in cyan.")
+	color.Blue("Prints %s in blue.", "text")
+
+	c := color.New(color.FgCyan).Add(color.Underline)
+	c.Println("Prints cyan text with an underline.")
+
+	// Or just add them to New()
+	d := color.New(color.FgCyan, color.Bold)
+	d.Printf("This prints bold cyan %s\n", "too!.")
+
+	// Mix up foreground and background colors, create new mixes!
+	red := color.New(color.FgRed)
+
+	boldRed := red.Add(color.Bold)
+	boldRed.Println("This will print text in bold red.")
+
+	whiteBackground := red.Add(color.BgWhite)
+	whiteBackground.Println("Red text with white background.")
+
+	// Use handy standard colors
+	color.Set(color.FgYellow)
+
+	fmt.Println("Existing text will now be in yellow")
+	fmt.Printf("This one %s\n", "too")
+
+	color.Unset() // Don't forget to unset
+
+	// You can mix up parameters
+	color.Set(color.FgMagenta, color.Bold)
+	defer color.Unset() // Use it in your function
+
+	fmt.Println("All text will now be bold magenta.")
 	return "Addon printed"
 }
